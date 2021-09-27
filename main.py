211faf852ed52622e96420de7b8409fdd4d9fcb7
eterazim <59294202+eterazim@users.noreply.github.com>
2021-09-27 10:59:50 -0400

    added more fuck you triggers

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 import music
-
-depressioncount = 0
-
 cogs = [music]
 
 client = commands.Bot(command_prefix='?', case_insensitive=True, intents = discord.Intents.all())
@@ -29,7 +26,6 @@ async def on_message(message):
 
       if 'depression' in message.content:
         await message.channel.send("stfu fertlizer")
-        depressioncount + 1
       await client.process_commands(message)
 
       if 'depression' in message.content and 'womens rights' in message.content:
@@ -44,10 +40,22 @@ async def on_message(message):
         await message.channel.send("Based")
       await client.process_commands(message)
 
+      if 'cum' in message.content:
+        await message.channel.send("Cum")
+      await client.process_commands(message)
+
+      if 'im happy' in message.content:
+        await message.channel.send("Stfu, fertlizer you're coping")
+      await client.process_commands(message)
+
+      if 'fuck you fertilizer' in message.content:
+        await message.channel.send("fuck fertlizer all my homies hate fertlizer")
+      await client.process_commands(message)
+
 
 for i in range(len(cogs)):
   cogs[i].setup(client)
 
        
-client.run("TOKEN GOES HERE (LMAO YOU THOUGHT)")
+client.run("ODg4MTU2MTU2NTU0NTM5MDA5.YUOl4A.11zvUODkXZt2zFKYa1uep0MQ09Q")
 
